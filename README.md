b82f69d6cffec9140c902172f02bdf6900bc87b4
Seongwon Oh <osw970325@naver.com>
2022-02-03 01:52:13 +0900

    init project
    추가한 라이브러리
    - spring boot starter web
    - spring boot devtools
    - spring data jpa
    - spring security
    - thymeleaf
    - lombok
    - h2 db

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Screen_Tool_With_Spring
+
 Electron_Screen-tool 리포지토리의 백엔드를 Spring 프레임워크로 재구축한 프로젝트입니다.
---
+++ src/main/java/com/project/screentool/ScreenToolApplication.java
@@ -0,0 +1,13 @@
+package com.project.screentool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScreenToolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScreenToolApplication.class, args);
+    }
+
+}
